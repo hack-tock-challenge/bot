682ce9966bfb66ade0867f7541efea3382851ac4
mathieu <msansen@gmail.com>
2019-11-22 14:16:39 +0100

    WIP questionsuivante not working

--- common/src/main/kotlin/bot.kt
+++ common/src/main/kotlin/bot.kt
@@ -60,7 +60,7 @@ val bot = newBot(
             map.clear()
             end(
                     newCard(
-                            "Bonjour, je suis Quizzy?",
+                            "Bonjour, je suis Quizzy :)",
                             "Les Office de Tourisme te proposent des questions sur ton parcours !",
                             newAttachment("https://zupimages.net/up/19/47/lern.png"),
                             newAction("Mode adulte"),
@@ -146,7 +146,24 @@ val bot = newBot(
             if ( indexCourse.toInt() < myList?.size!!){
 
                 val c = myList?.get(indexCourse.toInt())
-                if (c.image.isNullOrEmpty()){
+                val ch1= c?.choices?.get(0)?.text
+                val ch2= c?.choices?.get(1)?.text
+                val ch3= c?.choices?.get(2)?.text
+
+                if ( c?.type == "choices" ){
+                    //On retourne une carte a actions
+                    send (
+                            newCard(
+                            "$c?.messages[0]",
+                            "$c?.messages[1]",
+                            null,
+                            newAction("$ch1"),
+                            newAction("$ch2"),
+                            newAction("$ch3")
+                            )
+                    )
+                }
+                else if (c.image.isNullOrEmpty()){
                     for (item in c?.messages) {
                         send(item)
                     }
@@ -180,8 +197,16 @@ val bot = newBot(
             val entityType = c?.type?.let { entityText(it) }
             println(entityType)
             //test de la reponse
+
             if ( c?.answer ==  entityType ){
-                end("Bravo, c'est la bonne reponse")
+                if (c != null) {
+                    end(
+                            newCard(
+                                    c.congratMessage,null, null,
+                                    newAction("Question suivante")
+                            )
+                    )
+                }
             }
             else{
                 end(
